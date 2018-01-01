9538c515de543190b1cdf3d2ad28ed892f0c2a32
Murali <Murali@Harshi>
2018-01-01 20:59:24 +0530

    Second update

---
+++ src/main/java/APITesting/org/Parser/ABC.java
@@ -0,0 +1,21 @@
+package APITesting.org.Parser;
+
+abstract class ABC {
+	
+	public ABC()
+	{
+		System.out.println("Abstract class constructor");
+	}
+	
+	public void test1()
+	{
+		System.out.println("No arg method");
+	}
+	 public void test2()
+	{
+		System.out.println("No arg method");
+	}
+	
+	//abstract public void test3();
+
+}
